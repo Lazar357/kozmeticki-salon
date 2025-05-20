af0cf7091b24f8d6a09a10adfb0d47efed6a8fc5
Lazar357 <lazar.danic.23@singimail.rs>
2025-05-20 11:57:53 +0200

    Add basic layout

---
+++ src/main/java/rs/ac/singidunum/kozmeticki/salon/KozmetickiSalon.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package rs.ac.singidunum.kozmeticki.salon;
+
+/**
+ *
+ * @author PC
+ */
+public class KozmetickiSalon {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
