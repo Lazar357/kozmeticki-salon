9819c738a5c78d90b90abe34e954b720d3d7ef3d
Lazar357 <lazar.danic.23@singimail.rs>
2025-05-29 13:50:33 +0200

    Edited frontend

---
+++ src/main/java/rs/ac/singidunum/kozmeticki_salon/KozmetickiSalonApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum.kozmeticki_salon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KozmetickiSalonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KozmetickiSalonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/ac/singidunum/kozmeticki_salon/KozmetickiSalonApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum.kozmeticki_salon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KozmetickiSalonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
